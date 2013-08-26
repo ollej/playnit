6bc259265b211ca633e48b2d8018988e798e4036
Olle Johansson <olle@avidity.se>
2013-08-26 22:08:06 +0200

    Map support in index view.

--- db/migrate/20130826193714_add_lat_lng_to_playings.rb
+++ db/migrate/20130826193714_add_lat_lng_to_playings.rb
@@ -2,5 +2,6 @@ class AddLatLngToPlayings < ActiveRecord::Migration
   def change
     add_column :playings, :latitude, :decimal
     add_column :playings, :longitude, :decimal
+    Playing.where(latitude: nil).update_all(latitude: 0, longitude: 0)
   end
 end
