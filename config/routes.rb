72e940c0a5b20ba8358cde64447b84b52175afed
Olle Johansson <olle@avidity.se>
2013-10-28 21:07:01 +0100

    Changed root-path to playingsindex, updated gems and added jquery_ujs

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Playnit::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => "home#index"
+  root :to => "playings#index"
 
   # See how all your routes lay out with "rake routes"
 
