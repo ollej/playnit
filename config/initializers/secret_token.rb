0de6b88b471bf9b97f8272d2d765d2eb465cd2db
Olle Johansson <olle@avidity.se>
2013-11-01 15:08:29 +0100

    Secret token should not be in repo.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Playnit::Application.config.secret_token = '1c1a0f8a312b6557c3b9c6a3d441d1888644f7bfb369f62db52040787181a442e54e8c1774c7b38823b7ff2e1238050523f01e1257f966259ad9fbe16d31d275'
+Playnit::Application.config.secret_token = ENV['SECRET_TOKEN']
