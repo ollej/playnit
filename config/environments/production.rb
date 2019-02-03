4a7f94014d04958c2ee6317e8e62dd9d07744206
Olle Johansson <olle@wreede.se>
2019-02-03 18:48:45 +0100

    Tweak redis settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, ENV["REDIS_URL"], { expires_in: 90.minutes }
+  config.cache_store = :redis_store, "#{ENV["REDIS_URL"]}/cache", { expires_in: 90.minutes }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Playnit::Application.config.session_store :redis_store
+Playnit::Application.config.session_store :redis_store,
+  servers: ["#{ENV['REDIS_URL']}/session"],
+  expire_after: 90.minutes,
+  key: "_#{Rails.application.class.parent_name.downcase}_session",
+  threadsafe: false
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
