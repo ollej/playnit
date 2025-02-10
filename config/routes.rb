dde0f1456292d44e7a0cf321ec3f8daf6811db66
Olle Wreede <olle@wreede.se>
2025-02-10 17:45:42 +0100

    Move deploy check config to app.json

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,8 @@ Playnit::Application.routes.draw do
   #     resources :products
   #   end
 
+  get '/health', to: proc { [200, {}, ['success']] }
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
