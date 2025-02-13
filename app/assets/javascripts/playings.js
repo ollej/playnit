84e6dbd79f41dae0ea91279cf9d4c6def856bdd3
Olle Wreede <olle@wreede.se>
2025-02-13 21:12:54 +0100

    Decaffeinate playings.js

---
+++ app/assets/javascripts/playings.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  logger.info('Playings page');
+  new PlayingsGeo().initGeo();
+  new GameSelector();
+
+  if ($("#uploader").length > 0) {
+    logger.debug('Creating Uploader with options:', window.uploader_options);
+    new Uploader(window.uploader_options).init();
+  }
+});
--- app/assets/javascripts/playings.js.coffee
+++
@@ -1,8 +0,0 @@
-$(document).ready =>
-  logger.info 'Playings page'
-  new PlayingsGeo().initGeo()
-  gamesel = new GameSelector()
-
-  if $("#uploader").length > 0
-    logger.debug 'Creating Uploader with options:', window.uploader_options
-    new Uploader(window.uploader_options).init()
